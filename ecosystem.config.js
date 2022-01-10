db3309e3788f708ecfc1f1ee881bec1e597de512
GREEB <gr44bli@gmail.com>
2022-01-10 04:40:03 +0100

    auto deploy pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,5 +7,15 @@ module.exports = {
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
     }
-  ]
+  ],
+  deploy: {
+    production: {
+      user: 'GREEB',
+      host: '159.223.15.204',
+      ref: 'origin/main',
+      repo: 'git@github.com:GREEB/trakr.app.git',
+      path: '/opt/trakr.app',
+      'post-deploy': 'yarn build'
+    }
+  }
 }
