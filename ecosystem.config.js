f138c373bdf1629641f4548ad2e95120bcef092d
GREEB <gr44bli@gmail.com>
2022-01-12 05:52:11 +0100

    pm2 fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,13 @@ module.exports = {
     {
       name: 'trakr.app',
       script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start',
       env_production: {
         NODE_ENV: 'production'
       },
       env_development: {
         NODE_ENV: 'development'
-      },
-      args: 'NODE_ENV=production start'
+      }
     }
   ]
 }
