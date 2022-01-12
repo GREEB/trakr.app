7ad84f439208f3c86071b4b452bf60fc53f9358c
GREEB <gr44bli@gmail.com>
2022-01-12 06:24:16 +0100

    more deploy fixes maybe

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,23 @@
+/* eslint-disable quote-props */
+require('dotenv').config()
 module.exports = {
   apps: [
     {
-      name: 'trakr.app',
+      name: 'nuxtjs',
       script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start',
-      env_production: {
-        NODE_ENV: 'production'
-      },
-      env_development: {
-        NODE_ENV: 'development'
-      }
+      args: 'start'
     }
-  ]
+  ],
+  deploy: {
+    // "production" is the environment name
+    production: {
+      'user': 'root',
+      'host': [process.env.BOXIP],
+      'ref': 'origin/master',
+      'repo': 'git@github.com:GREEB/trakr.app.git',
+      'ssh_options': ['ForwardAgent=yes'],
+      'path': '/opt/trakr.app',
+      'post-deploy': 'yarn && yarn build && pm2 startOrRestart ecosystem.config.js --env production'
+    }
+  }
 }
