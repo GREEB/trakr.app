a1b1a83d5c017c5f36fd92f57f9e5787ede0e543
GREEB <gr44bli@gmail.com>
2022-01-17 16:21:13 +0100

    feat(connectivity): show tooltip to see connectivity

--- components/Appbar.vue
+++ components/Appbar.vue
@@ -4,32 +4,58 @@
     fixed
     app
     dense
-    rounded
     elevation="1"
   >
-    <v-tooltip v-model="showConnectionTooltip" bottom>
-      <template #activator="{ on, attrs }">
-        <img
-          v-bind="attrs"
-          class="logo"
-          src="~static/logo512.png"
-          :class="connected && onlineClass"
-          v-on="on"
-        >
-      </template>
-      <span>{{ connectedText }}</span>
-    </v-tooltip>
     <NuxtLink class="tb-icon" to="/">
-      <v-toolbar-title v-text="title" />
+      <v-tooltip v-model="showConnectionTooltip" bottom color="#000">
+        <template #activator="{ on, attrs }">
+          <v-img
+            max-height="40"
+            max-width="30"
+            v-bind="attrs"
+            class="logo"
+            src="/logo512.png"
+            :class="connected && onlineClass"
+            v-on="on"
+          />
+        </template>
+        <span class="connectionTooltip">
+          <span :class="connected && 'connected'">Sockets</span><br>
+          <span :class="isAuthenticated && 'connected'">Auth</span>
+        </span>
+      </v-tooltip>
     </NuxtLink>
+    <v-toolbar-title v-text="title" />
+
     <v-btn
-      class="ml-5"
+      class="ml-5 about"
       text
       small
+      depressed
+      to="/"
+    >
+      home
+    </v-btn>
+    <v-btn
+      class="ml-5 about"
+      text
+      small
+      depressed
       to="/about"
     >
       About
     </v-btn>
+    <v-btn
+      class="ml-5 about"
+      text
+      small
+      depressed
+      to="/hello"
+    >
+      Getting started
+    </v-btn>
+    <v-spacer />
+
     <!-- <v-menu offset-y>
       <template #activator="{ on, attrs }">
         <v-btn
@@ -55,28 +81,60 @@
     </v-menu> -->
     <v-spacer />
     <!-- Not sure how to fix this error the right way -->
-    <!-- <client-only>
-      <v-btn
+    <client-only>
+      <v-dialog
         v-if="!$auth.loggedIn"
-        color="blue darken-1"
-        text
-        class="ma-2 white--text"
-        :href="discordURL"
+        v-model="dialog"
+        scrollable
+        max-width="20em"
+        persistent
       >
-        Login
-        <v-icon
-          right
-          dark
-        >
-          mdi-discord
-        </v-icon>
-        <v-icon
-          dark
-          right
-        >
-          mdi-open-in-new
-        </v-icon>
-      </v-btn>
+        <template #activator="{ on, attrs }">
+          <v-btn
+            color="blue darken-1"
+            text
+            class="ma-2 white--text"
+            v-bind="attrs"
+            v-on="on"
+          >
+            Login
+            <v-icon
+              right
+              dark
+            >
+              mdi-discord
+            </v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>Privacy Policy for trakr.app</v-card-title>
+          <v-divider />
+          <vue-scroll :ops="ops">
+            <v-card-text style="height: 300px;">
+              <PrivacyPolicy />
+            </v-card-text>
+          </vue-scroll>
+          <v-divider />
+          <v-card-actions>
+            <v-spacer />
+            <v-btn
+              color="grey darken-1"
+              text
+              @click="dialog = false"
+            >
+              Disagree
+            </v-btn>
+            <v-btn
+              color="green"
+              text
+              :href="discordURL"
+              @click="dialog = false"
+            >
+              Agree
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
       <v-menu
         v-else
         bottom
@@ -88,6 +146,7 @@
           <v-btn
             small
             icon
+            class="mr-2"
             v-on="on"
           >
             <v-avatar
@@ -104,7 +163,7 @@
           <v-list-item-content class="justify-center">
             <div class="mx-auto text-center">
               <h3>{{ $auth.user.username }}</h3>
-              <v-divider class="my-3" />
+              <!-- <v-divider class="my-3" />
               <v-btn
                 depressed
                 rounded
@@ -112,26 +171,37 @@
               >
                 Edit Account
               </v-btn>
+               -->
               <v-divider class="my-3" />
               <v-btn
                 depressed
-                rounded
                 text
                 @click="logout"
               >
-                Disconnect
+                Logout
               </v-btn>
             </div>
           </v-list-item-content>
         </v-card>
       </v-menu>
-    </client-only> -->
+    </client-only>
   </v-app-bar>
 </template>
 <script>
 
 export default {
   data: () => ({
+    ops: {
+      bar: {
+        onlyShowBarOnScroll: true,
+        keepShow: false,
+        background: '#151515 ',
+        opacity: 1,
+        minSize: 0,
+        size: '6px'
+      }
+    },
+    dialog: false,
     title: 'trakr.app',
     onlineClass: 'online',
     gg: null,
@@ -141,6 +211,9 @@ export default {
     oauth: 'https://discord.com/api/oauth2/authorize?client_id=918603750057328640&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&response_type=code&scope=identify%20email'
   }),
   computed: {
+    bg () {
+      return this.$vuetify.theme.themes
+    },
     discordURL () {
       const baseurl = 'https://discord.com/api/oauth2/authorize'
       const clientID = '?client_id=' + this.$config.discordId
@@ -163,10 +236,15 @@ export default {
   },
   watch: {
     connected (val) {
-      if (val) {
-        this.connectedText = 'Connected'
+      console.log(this.isAuthenticated)
+      if (val && this.isAuthenticated) {
+        this.connectedText = 'sockets, auth'
+      } else if (val && !this.isAuthenticated) {
+        this.connectedText = 'sockets, !auth'
+      } else if (val) {
+        this.connectedText = 'sockets'
       } else {
-        this.connectedText = 'Disconncted'
+        this.connectedText = '!sockets'
       }
 
       this.showConnectionTooltip = true
@@ -174,13 +252,6 @@ export default {
         this.showConnectionTooltip = false
       }, 1000)
     }
-  },
-  created () {
-    setInterval(() => {
-      if (this.pc) {
-        console.log(this.count)
-      }
-    }, 1000)
   },
   methods: {
     async logout () {
@@ -192,12 +263,30 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+.connectionTooltip
+  span
+    &::before
+      content: ""
+      height: 8px
+      width: 8px
+      border-radius: 100%
+      background-color: #dd2c00
+      position: absolute
+      margin-left: -12px
+      margin-top: 6px
+.connectionTooltip
+  .connected
+      &::before
+        background-color: #00e676
+
+// .about.theme--dark.v-btn--active:hover::before, .about.theme--dark.v-btn--active::before
+//     opacity: 0
 .tb-icon
   font-weight: 600
   text-decoration: none
   color: #fff
 .logo
-    transition: all 0.5s ease
+    transition: all 2s ease
     filter: grayscale(1)
     margin-top: 0.3em
 .logo
