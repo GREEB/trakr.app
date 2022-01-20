00aa655700741f6d08d5df38102dacd341991401
GREEB <gr44bli@gmail.com>
2022-01-20 03:00:09 +0100

    deploy script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,6 @@ module.exports = {
       host: [process.env.BOXIP],
       ref: 'origin/master',
       repo: 'git@github.com:GREEB/trakr.app.git',
-      ssh_options: ['ForwardAgent=yes'],
       path: '/opt/trakr.app',
       'post-deploy': 'yarn && yarn build && pm2 startOrRestart ecosystem.config.js --env production'
     }
