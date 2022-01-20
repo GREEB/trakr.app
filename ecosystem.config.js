8e7186c172df74348f85bb20b21e22ec1a7c6931
GREEB <gr44bli@gmail.com>
2022-01-20 03:47:51 +0100

    it was master/main error omg

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,10 +12,9 @@ module.exports = {
     production: {
       user: 'root',
       host: [process.env.BOXIP],
-      ref: 'origin/master',
+      ref: 'origin/main',
       repo: 'git@github.com:GREEB/trakr.app.git',
       path: '/opt/trakr.app',
-      key: '~/.ssh/id_rsa.pem',
       'post-deploy': 'yarn && yarn build && pm2 startOrRestart ecosystem.config.js --env production'
     }
   }
