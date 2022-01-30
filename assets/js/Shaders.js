014bda9f21089bac8e2ac46d7b2b3ee0ba27a2a8
GREEB <gr44bli@gmail.com>
2022-01-30 23:35:23 +0100

    smaller points to have the heatmap effect

--- assets/js/Shaders.js
+++ assets/js/Shaders.js
@@ -1,3 +1,9 @@
+export const defaultFragment = `varying vec3 vColor;
+varying vec3 vPos;
+void main() {
+    gl_FragColor = vec4((vPos.z / 50.0) * 3.0,0.51,0.51, 1.0);
+}
+`
 export const defaultVertex = `attribute float size;
 varying vec3 vColor;
 varying vec3 vPos;
@@ -5,12 +11,6 @@ void main() {
     vColor = color;
     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
     vPos = position;
-    gl_PointSize = color.r * 5.0;
-}
-`
-export const defaultFragment = `varying vec3 vColor;
-varying vec3 vPos;
-void main() {
-    gl_FragColor = vec4((vPos.z / 50.0) * 3.0,0.51,0.51, 1.0);
+    gl_PointSize = color.r * 1.0;
 }
 `
