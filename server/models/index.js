02e9aa152d75fce919c5f5ca7f250f6d78bbdf1f
GREEB <gr44bli@gmail.com>
2022-02-02 06:25:31 +0100

    more db settings stuff

--- server/models/index.js
+++ server/models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
 // eslint-disable-next-line node/no-path-concat
-const config = require(__dirname + '/../config/config.json')[env]
+const config = require(__dirname + '/../config/config.js')[env]
 const db = {}
 
 let sequelize
