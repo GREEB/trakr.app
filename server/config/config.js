0ab67ffcc5cb4763e873492b2c99af67943bf421
GREEB <gr44bli@gmail.com>
2022-02-02 06:22:16 +0100

    db deploy

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  development: {
+    use_env_variable: 'POSTGRES',
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'POSTGRES',
+    dialect: 'postgres',
+    logging: false
+  }
+}
